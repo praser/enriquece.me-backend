2e57db8de2857de7ddf44490b684a47a0943a854
Rubens Praser Júnior <praser@gmail.com>
2017-04-05 09:04:24 -0300

    Create User model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User
+	include Mongoid::Document
+	include ActiveModel::SecurePassword
+
+	field :name, type: String
+	field :email, type: String
+	field :password_digest, type: String
+
+	has_secure_password
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: 'Email seems to be invalid'}
+end
