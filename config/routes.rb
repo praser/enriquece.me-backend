fcc73bcbc1ba2fe3a18d08e79777c0e0f81271d7
Rubens Praser Júnior <praser@gmail.com>
2017-04-06 06:41:23 -0300

    Add API version v1 generated with versionist

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # TODO: Before create any route, see versionist documentation in https://github.com/bploetz/versionist in order to create versioned routos of the API.
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# TODO: Before create any route, see versionist documentation in https://github.com/bploetz/versionist in order to create versioned routos of the API.
+	api_version(:module => "v1", :path => {:value => "v1"}, :defaults => {:format => "json"}, :default => true) do
+	end
 end
