eb8eac14402083c674207d173322b61b5f2341c0
Rubens Praser Júnior <praser@gmail.com>
2017-04-09 16:30:58 -0300

    Remove subdomain from api_version declaration in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-	api_version(module: "v1", subdomain: "api", path: {value: "v1"}, defaults: {format: "json"}, default: true) do
+	api_version(module: "v1", path: {value: "v1"}, defaults: {format: "json"}, default: true) do
     	post 'authenticate', to: 'authentications#create'
     	resources :users
 	end
