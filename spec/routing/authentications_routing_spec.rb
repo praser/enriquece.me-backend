87bd3dd68a7d7ce60b2f3d89311932b07836d945
Rubens Praser Júnior <praser@gmail.com>
2017-04-13 07:34:18 -0300

    Test for authentication default route

---
+++ spec/routing/authentications_routing_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe V1::AuthenticationsController, type: :routing do
+  describe "routing" do
+  	let(:default_version) {"v1"}
+
+    it "routes to #create" do
+      expect(:post => "authenticate").to route_to(
+        controller: "#{default_version}/authentications",
+        action: 'create',
+        format: 'json'
+      )
+    end
+  end
+end
