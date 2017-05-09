bad416441a6e086b093df9b4566c22ed589a5336
Rubens Praser Júnior <praser@gmail.com>
2017-05-09 10:20:41 -0300

    Add subcategory remove functionality

--- features/step_definitions/subcategory.rb
+++ features/step_definitions/subcategory.rb
@@ -29,12 +29,29 @@ Quando(/^o backend receber uma requisição autenticada para alterar a subcatego
   put default_subcategory_path(subcategory), params.to_json.to_s
 end
 
+Quando(/^o backend receber uma requisição autenticada para remover a subcategoria "([^"]*)"$/) do |subcategory_name|
+  header "Content-Type", "application/vnd.api+json"
+  header "Authorization", "Bearer #{@token}"
+
+  subcategory = Subcategory.find_by(name: subcategory_name)
+
+  delete default_subcategory_path(subcategory)
+end
+
 Quando(/^o backend receber uma requisição não autenticada para alterar a subcategoria "([^"]*)"$/) do |subcategory_name|
   header "Content-Type", "application/vnd.api+json"
 
   subcategory = Subcategory.find_by(name: subcategory_name)
 
   put default_subcategory_path(subcategory)
+end
+
+Quando(/^o backend receber uma requisição não autenticada para remover a subcategoria "([^"]*)"$/) do |subcategory_name|
+  header "Content-Type", "application/vnd.api+json"
+
+  subcategory = Subcategory.find_by(name: subcategory_name)
+
+  delete default_subcategory_path(subcategory)
 end
 
 Então(/^a subcategoria "([^"]*)" deve ser cadastrada$/) do |category_name|
@@ -65,4 +82,8 @@ Então(/^o campo "([^"]*)" da subcategoria deve ser "([^"]*)"$/) do |field, valu
   end
 
   expect(body['data']['attributes'][field].to_s).to eq value.to_s
+end
+
+Então(/^a subcategoria "([^"]*)" deverá ter sido removida$/) do |subcategory_name|
+  expect(Subcategory.find_by(name: subcategory_name)).to be_nil
 end
