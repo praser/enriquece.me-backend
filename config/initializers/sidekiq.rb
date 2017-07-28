69392ce2a48598923b788ed1e80de4c218255ca7
Rubens Praser Júnior <praser@gmail.com>
2017-07-28 10:06:27 -0300

    Config: add Sidekiq configs to wort properly in heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,3 +4,11 @@ require 'sidekiq/web'
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["enriquecame", "3nr1qu3c4m3"]
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 9 }
+end
